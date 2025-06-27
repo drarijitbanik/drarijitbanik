a4e096b018c0864a63dd3b1d687743179a6d6ca5
suman-ops-git <sumanghosh613@gmail.com>
2025-06-27 21:59:33 +0530

    added

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/doctor-arijit-banik'),
+      dir: require('path').join(__dirname, './coverage/drarijitbanik'),
       subdir: '.',
       reporters: [
         { type: 'html' },
